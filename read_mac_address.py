9eee95c6f32ed5ae219dad4cb17da8a8d9bc2604
ehrentreu <53800301+ehrentreu@users.noreply.github.com>
2019-12-31 11:37:57 +0200

    which_driver_sleeps

--- read_mac_address.py
+++ read_mac_address.py
@@ -1,17 +1,12 @@
-import requests
-import datetime
-from datetime import timedelta
 
 def get_mac():
     with open ("lsOutput.log", 'r') as get_mac:
         for line in get_mac:
             if "ether" in line:
                 mac_address = line.split()[1].strip()
-                time = datetime.datetime.now() + timedelta(hours=2)
-                post_data = {'mac_address': mac_address, 'time': time}
-                print(post_data)
-                ans = requests.post("http://10.144.68.104:5000/add_record", data=post_data)
-                print(ans)
-                print(ans.text)
-                return mac_address
+                with open("mac_address.txt", 'w') as save_mac_address:
+                    save_mac_address.write(mac_address)
+                # print(mac_address)
+                # return mac_address
+
 get_mac()
